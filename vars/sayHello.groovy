610806d1616f9f208ade4d0415821ba8c5b62809
deladev <deladev@protonmail.com>
2024-04-03 12:12:05 +0200

    syntax

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,12 +1,12 @@
 def call(String name = 'human') {
     pipeline {
-    agent any
-    stages {
-        stage('Initial stage') {
-        steps {
-            sh 'echo Hello ${name}'
+        agent any
+        stages {
+            stage('Initial stage') {
+                steps {
+                    sh 'echo Hello ${name}'
+                }
+            }
         }
-        }
-    }
     }
 }
