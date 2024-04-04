5aeecb52e0d49529f30cd3ddcaba103712712306
deladev <deladev@protonmail.com>
2024-04-04 11:35:56 +0200

    Disable some code

--- vars/deployAxualTerraform.groovy
+++ vars/deployAxualTerraform.groovy
@@ -1,31 +1,37 @@
-def call(String cloud_credentials_id='') {
+def call(String cloudCredentialsID='') {
     pipeline {
         environment {
-            ARM = credentials("${cloud_credentials_id}")
+            ARM = credentials("${cloudCredentialsID}")
         }
         agent any
         stages {
-            stage('Initialize') {
+            stage('Validate & Deploy: Ontw') {
                 steps {
-                    sh 'git clone https://github.com/jdelgit/terraform-modules.git'
-                    sh 'terraform -chdir=./test init --backend-config backend.conf'
+                    echo 'Check out Axual module'
                 }
             }
-            stage('Plan') {
-                steps {
-                    sh 'terraform -chdir=./test plan -out test.plan'
-                }
-            }
-            stage('Deploy approval'){
-                steps {
-                    input "Deploy to environment?"
-                }
-            }
-            stage('Apply plan') {
-                    steps {
-                        sh 'terraform -chdir=./test apply test.plan'
-                    }
-            }
+
+            // stage('Initialize') {
+            //     steps {
+            //         sh 'git clone https://github.com/jdelgit/terraform-modules.git'
+            //         sh 'terraform -chdir=./test init --backend-config backend.conf'
+            //     }
+            // }
+            // stage('Plan') {
+            //     steps {
+            //         sh 'terraform -chdir=./test plan -out test.plan'
+            //     }
+            // }
+            // stage('Deploy approval') {
+            //     steps {
+            //         input 'Deploy to environment?'
+            //     }
+            // }
+            // stage('Apply plan') {
+            //         steps {
+            //             sh 'terraform -chdir=./test apply test.plan'
+            //         }
+            // }
         }
     }
 }
