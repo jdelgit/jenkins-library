661e61df10d4cee9065207e92bacadce52190a24
deladev <deladev@protonmail.com>
2024-04-04 18:26:13 +0200

    one more time

--- vars/deployAxualTerraform.groovy
+++ vars/deployAxualTerraform.groovy
@@ -1,6 +1,6 @@
 
 def call(String cloudCredentialsID='') {
-    // String validationScriptPath = libraryResource('/com/test/python/validation.py')
+    // String validationScriptPath = libraryResource('resources/example/validation.py')
 
     pipeline {
         environment {
@@ -10,7 +10,7 @@ def call(String cloudCredentialsID='') {
         stages {
             stage('Validate & Deploy: Ontw') {
                 steps {
-                    sh "python3 ${libraryResource('/com/test/python/validation.py')} test"
+                    sh "python3 ${libraryResource('resources/example/validation.py')} test"
                 }
             }
 
